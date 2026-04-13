85beaa9cb0df39775de9a66bb6cf6558a0f6b9e0
astrnvk <61580599+astrnvk@users.noreply.github.com>
2026-04-13 23:33:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+class TestPlugin extends Plugin {
+  async onload() {
+    console.log('Test plugin loaded!');
+    this.addRibbonIcon('star', 'Test Plugin', () => {
+      alert('Test plugin clicked!');
+    });
+  }
+  
+  onunload() {
+    console.log('Test plugin unloaded!');
+  }
+}
 
+module.exports = TestPlugin;
